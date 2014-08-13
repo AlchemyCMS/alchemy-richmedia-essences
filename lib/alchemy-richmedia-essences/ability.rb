59d7a0fb38e48cb183dfb1511255b656645953a0
Thomas von Deyen <tvd@magiclabs.de>
2014-08-14 01:13:23 +0200

    Use modules as namespace not constants
    This should fix #2

--- lib/alchemy-richmedia-essences/ability.rb
+++ lib/alchemy-richmedia-essences/ability.rb
@@ -1,13 +1,18 @@
-class Alchemy::Richmedia::Essences::Ability
-  include CanCan::Ability
+module Alchemy
+  module Richmedia
+    module Essences
+      class Ability
+        include CanCan::Ability
 
-  def initialize(user)
-    return if user.nil?
-    if user.has_role?(:author) || user.has_role?(:editor) || user.has_role?(:admin)
-      can :manage, Alchemy::EssenceAudio
-      can :manage, Alchemy::EssenceFlash
-      can :manage, Alchemy::EssenceVideo
+        def initialize(user)
+          return if user.nil?
+          if user.has_role?(:author) || user.has_role?(:editor) || user.has_role?(:admin)
+            can :manage, Alchemy::EssenceAudio
+            can :manage, Alchemy::EssenceFlash
+            can :manage, Alchemy::EssenceVideo
+          end
+        end
+      end
     end
   end
-
 end
