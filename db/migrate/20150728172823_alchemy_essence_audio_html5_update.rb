336c512708323f131e8bb39db5c28d2b89b79b49
Thomas von Deyen <tvd@magiclabs.de>
2015-07-28 17:52:58 +0200

    Update EssenceAudio to HTML5

---
+++ db/migrate/20150728172823_alchemy_essence_audio_html5_update.rb
@@ -0,0 +1,17 @@
+class AlchemyEssenceAudioHtml5Update < ActiveRecord::Migration
+  def up
+    change_table :alchemy_essence_audios do |t|
+      t.boolean :autoplay, default: false
+      t.boolean :loop,     default: false
+      t.boolean :muted,    default: false
+      t.rename :show_navigation, :controls
+      t.remove :width
+      t.remove :height
+      t.remove :show_eq
+    end
+  end
+
+  def down
+    raise ActiveRecord::IrreversibleMigration
+  end
+end
